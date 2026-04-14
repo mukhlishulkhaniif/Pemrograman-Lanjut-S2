dee3b219dfe3c91153a2ff7282490d1391c42ea1
mukhlishulkhaniif <mukhlishulkhaniif@gmail.com>
2026-04-14 20:08:48 +0700

    Menambahkan Kalkulator
    Menambahkan project Kalkulator ke repositori Pemrograman-Lanjut-S2

---
+++ Kalkulator/Form1.Designer.cs
@@ -0,0 +1,310 @@
+﻿namespace Kalkulator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtDisplay = new System.Windows.Forms.TextBox();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.btn0 = new System.Windows.Forms.Button();
+            this.btnKurang = new System.Windows.Forms.Button();
+            this.btnTambah = new System.Windows.Forms.Button();
+            this.btnBagi = new System.Windows.Forms.Button();
+            this.btnKali = new System.Windows.Forms.Button();
+            this.btnPersen = new System.Windows.Forms.Button();
+            this.btnHasil = new System.Windows.Forms.Button();
+            this.btnKoma = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtDisplay
+            // 
+            this.txtDisplay.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDisplay.Location = new System.Drawing.Point(12, 12);
+            this.txtDisplay.Name = "txtDisplay";
+            this.txtDisplay.Size = new System.Drawing.Size(418, 86);
+            this.txtDisplay.TabIndex = 0;
+            this.txtDisplay.Text = "0";
+            this.txtDisplay.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.txtDisplay.TextChanged += new System.EventHandler(this.txtDisplay_TextChanged);
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(12, 422);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(100, 100);
+            this.btn1.TabIndex = 1;
+            this.btn1.Text = "1";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.Angka_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(224, 422);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(100, 100);
+            this.btn3.TabIndex = 2;
+            this.btn3.Text = "3";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.Angka_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(118, 422);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(100, 100);
+            this.btn2.TabIndex = 3;
+            this.btn2.Text = "2";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.Angka_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.Location = new System.Drawing.Point(12, 316);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(100, 100);
+            this.btn4.TabIndex = 5;
+            this.btn4.Text = "4";
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.Angka_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.Location = new System.Drawing.Point(118, 210);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(100, 100);
+            this.btn8.TabIndex = 6;
+            this.btn8.Text = "8";
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.Angka_Click);
+            // 
+            // btn9
+            // 
+            this.btn9.Location = new System.Drawing.Point(224, 210);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(100, 100);
+            this.btn9.TabIndex = 7;
+            this.btn9.Text = "9";
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.Angka_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.Location = new System.Drawing.Point(12, 210);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(100, 100);
+            this.btn7.TabIndex = 8;
+            this.btn7.Text = "7";
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.Angka_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.Location = new System.Drawing.Point(118, 316);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(100, 100);
+            this.btn5.TabIndex = 9;
+            this.btn5.Text = "5";
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.Angka_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.Location = new System.Drawing.Point(224, 316);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(100, 100);
+            this.btn6.TabIndex = 10;
+            this.btn6.Text = "6";
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.Angka_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(12, 104);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(100, 100);
+            this.btnClear.TabIndex = 11;
+            this.btnClear.Text = "C";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClearAll_Click);
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(118, 104);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(100, 100);
+            this.btnBack.TabIndex = 12;
+            this.btnBack.Text = "Del";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnHapus1_Click);
+            // 
+            // btn0
+            // 
+            this.btn0.Location = new System.Drawing.Point(118, 528);
+            this.btn0.Name = "btn0";
+            this.btn0.Size = new System.Drawing.Size(100, 100);
+            this.btn0.TabIndex = 13;
+            this.btn0.Text = "0";
+            this.btn0.UseVisualStyleBackColor = true;
+            this.btn0.Click += new System.EventHandler(this.Angka_Click);
+            // 
+            // btnKurang
+            // 
+            this.btnKurang.Location = new System.Drawing.Point(330, 316);
+            this.btnKurang.Name = "btnKurang";
+            this.btnKurang.Size = new System.Drawing.Size(100, 100);
+            this.btnKurang.TabIndex = 14;
+            this.btnKurang.Text = "-";
+            this.btnKurang.UseVisualStyleBackColor = true;
+            this.btnKurang.Click += new System.EventHandler(this.Operasi_Click);
+            // 
+            // btnTambah
+            // 
+            this.btnTambah.Location = new System.Drawing.Point(330, 210);
+            this.btnTambah.Name = "btnTambah";
+            this.btnTambah.Size = new System.Drawing.Size(100, 100);
+            this.btnTambah.TabIndex = 15;
+            this.btnTambah.Text = "+";
+            this.btnTambah.UseVisualStyleBackColor = true;
+            this.btnTambah.Click += new System.EventHandler(this.Operasi_Click);
+            // 
+            // btnBagi
+            // 
+            this.btnBagi.Location = new System.Drawing.Point(330, 104);
+            this.btnBagi.Name = "btnBagi";
+            this.btnBagi.Size = new System.Drawing.Size(100, 100);
+            this.btnBagi.TabIndex = 16;
+            this.btnBagi.Text = "/";
+            this.btnBagi.UseVisualStyleBackColor = true;
+            this.btnBagi.Click += new System.EventHandler(this.Operasi_Click);
+            // 
+            // btnKali
+            // 
+            this.btnKali.Location = new System.Drawing.Point(224, 104);
+            this.btnKali.Name = "btnKali";
+            this.btnKali.Size = new System.Drawing.Size(100, 100);
+            this.btnKali.TabIndex = 17;
+            this.btnKali.Text = "x";
+            this.btnKali.UseVisualStyleBackColor = true;
+            this.btnKali.Click += new System.EventHandler(this.Operasi_Click);
+            // 
+            // btnPersen
+            // 
+            this.btnPersen.Location = new System.Drawing.Point(330, 422);
+            this.btnPersen.Name = "btnPersen";
+            this.btnPersen.Size = new System.Drawing.Size(100, 100);
+            this.btnPersen.TabIndex = 18;
+            this.btnPersen.Text = "%";
+            this.btnPersen.UseVisualStyleBackColor = true;
+            this.btnPersen.Click += new System.EventHandler(this.Operasi_Click);
+            // 
+            // btnHasil
+            // 
+            this.btnHasil.Location = new System.Drawing.Point(224, 528);
+            this.btnHasil.Name = "btnHasil";
+            this.btnHasil.Size = new System.Drawing.Size(200, 100);
+            this.btnHasil.TabIndex = 19;
+            this.btnHasil.Text = "=";
+            this.btnHasil.UseVisualStyleBackColor = true;
+            this.btnHasil.Click += new System.EventHandler(this.btnHasil_Click);
+            // 
+            // btnKoma
+            // 
+            this.btnKoma.Location = new System.Drawing.Point(12, 528);
+            this.btnKoma.Name = "btnKoma";
+            this.btnKoma.Size = new System.Drawing.Size(100, 100);
+            this.btnKoma.TabIndex = 20;
+            this.btnKoma.Text = ".";
+            this.btnKoma.UseVisualStyleBackColor = true;
+            this.btnKoma.Click += new System.EventHandler(this.Angka_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(438, 635);
+            this.Controls.Add(this.btnKoma);
+            this.Controls.Add(this.btnHasil);
+            this.Controls.Add(this.btnPersen);
+            this.Controls.Add(this.btnKali);
+            this.Controls.Add(this.btnBagi);
+            this.Controls.Add(this.btnTambah);
+            this.Controls.Add(this.btnKurang);
+            this.Controls.Add(this.btn0);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.txtDisplay);
+            this.Name = "Form1";
+            this.Text = "Kalkulator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtDisplay;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Button btn0;
+        private System.Windows.Forms.Button btnKurang;
+        private System.Windows.Forms.Button btnTambah;
+        private System.Windows.Forms.Button btnBagi;
+        private System.Windows.Forms.Button btnKali;
+        private System.Windows.Forms.Button btnPersen;
+        private System.Windows.Forms.Button btnHasil;
+        private System.Windows.Forms.Button btnKoma;
+    }
+}
+
---
+++ Kalkulator/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+
+namespace Kalkulator
+{
+    public partial class Form1 : Form
+    {
+        // Ingatan komputer untuk mengecek: Apakah kita baru saja menekan tombol Sama Dengan (=)?
+        bool sudahAdaHasil = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) { }
+        private void txtDisplay_TextChanged(object sender, EventArgs e) { }
+
+        // --- 1. FUNGSI UNTUK TOMBOL SAMA DENGAN (=) ---
+        private void btnHasil_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Langkah 1: Baca tulisan yang ada di layar kalkulator saat ini
+                string tulisanDiLayar = txtDisplay.Text;
+
+                // Langkah 2: Komputer tidak paham koma (,) untuk desimal dan 'x' untuk kali.
+                // terjemahkan dulu ke bahasa komputer:
+                //tulisanDiLayar = tulisanDiLayar.Replace(",", "."); // Ubah koma menjadi titik
+                tulisanDiLayar = tulisanDiLayar.Replace("x", "*"); // Ubah huruf x menjadi bintang (*)
+
+                // Langkah 3: pinjam "mesin penghitung" otomatis bawaan C# (namanya DataTable)
+                DataTable mesinPenghitung = new DataTable();
+
+                // Langkah 4: Suruh mesinnya menghitung tulisan yang sudah diterjemahkan tadi
+                object hasilHitungan = mesinPenghitung.Compute(tulisanDiLayar, "");
+
+                // Langkah 5: Ubah hasil hitungan menjadi angka biasa
+                double hasilAngka = Convert.ToDouble(hasilHitungan);
+
+                // Langkah 6: Ubah lagi menjadi teks yang rapi agar bisa ditampilin di layar
+                string teksHasil = hasilAngka.ToString(System.Globalization.CultureInfo.InvariantCulture);
+
+                // Langkah 7: Terjemahkan balik dari bahasa komputer (titik) ke bahasa kita (koma)
+                teksHasil = teksHasil.Replace(".", ",");
+
+                // Langkah 8: Tampilkan hasil akhirnya ke layar!
+                txtDisplay.Text = teksHasil;
+
+                // Langkah 9: Ingatkan komputer bahwa angka yang tampil sekarang adalah hasil akhir
+                sudahAdaHasil = true;
+            }
+            catch
+            {
+                // Kalau hitungannya ngawur (misal ngetik "5 + + 2"), tampilkan tulisan Error
+                txtDisplay.Text = "Error";
+                sudahAdaHasil = true;
+            }
+        }
+
+        // --- 2. FUNGSI UNTUK TOMBOL ANGKA (0 SAMPAI 9) DAN KOMA ---
+        private void Angka_Click(object sender, EventArgs e)
+        {
+            // Kita cek dulu kondisi layar saat ini
+            bool layarMenampilkanNol = (txtDisplay.Text == "0");
+            bool layarMenampilkanError = (txtDisplay.Text == "Error");
+
+            // Kalau di layar cuma ada angka 0, ATAU ada tulisan Error, ATAU baru saja klik "=",
+            // maka kita harus mengosongkan layar dulu sebelum ngetik angka baru.
+            if (layarMenampilkanNol || layarMenampilkanError || sudahAdaHasil)
+            {
+                txtDisplay.Text = ""; // Kosongkan layar
+                sudahAdaHasil = false; // Lupakan hasil yang lama
+            }
+
+            // Cari tahu tombol angka mana yang baru saja diklik olehmu
+            Button tombolAngka = (Button)sender;
+
+            // Tambahkan angka dari tombol itu ke layar
+            txtDisplay.Text = txtDisplay.Text + tombolAngka.Text;
+        }
+
+        // --- 3. FUNGSI UNTUK TOMBOL TAMBAH, KURANG, KALI, BAGI, PERSEN ---
+        private void Operasi_Click(object sender, EventArgs e)
+        {
+            // Kalau layar lagi error, ubah jadi 0 dulu biar aplikasinya tidak macet
+            if (txtDisplay.Text == "Error")
+            {
+                txtDisplay.Text = "0";
+            }
+
+            // Cari tahu tombol simbol mana yang diklik
+            Button tombolOperasi = (Button)sender;
+
+            // Tambahkan simbol operasi itu ke layar
+            txtDisplay.Text = txtDisplay.Text + tombolOperasi.Text;
+
+            // Beritahu komputer bahwa kita sedang lanjut merangkai rumus, bukan menampilkan hasil akhir
+            sudahAdaHasil = false;
+        }
+
+        // --- 4. FUNGSI UNTUK TOMBOL BERSIHKAN SEMUA (C) ---
+        private void btnClearAll_Click(object sender, EventArgs e)
+        {
+            // Kembalikan layar jadi angka 0
+            txtDisplay.Text = "0";
+
+            // Lupakan status hasil hitungan
+            sudahAdaHasil = false;
+        }
+
+        // --- 5. FUNGSI UNTUK TOMBOL HAPUS SATU HURUF (DEL / BACKSPACE) ---
+        private void btnHapus1_Click(object sender, EventArgs e)
+        {
+            // Kalau di layar ada tulisan Error atau angka hasil akhir, tombol hapus kita matikan
+            if (sudahAdaHasil == true || txtDisplay.Text == "Error")
+            {
+                return; // Return artinya: "Berhenti di sini, jangan lanjut kerjakan kode ke bawah"
+            }
+
+            // Hitung ada berapa banyak huruf/angka di layar saat ini
+            int jumlahHurufDiLayar = txtDisplay.Text.Length;
+
+            // Kalau hurufnya masih lebih dari satu
+            if (jumlahHurufDiLayar > 1)
+            {
+                // Ambil dan simpan semua huruf KECUALI satu huruf yang paling belakang
+                txtDisplay.Text = txtDisplay.Text.Substring(0, jumlahHurufDiLayar - 1);
+            }
+            else
+            {
+                // Kalau hurufnya tinggal sisa satu dan dihapus, jadikan layarnya angka 0
+                txtDisplay.Text = "0";
+            }
+        }
+    }
+}
