474abfdcb7e87e02e9a45017f93c82176b6f9303
mukhlishulkhaniif <mukhlishulkhaniif@gmail.com>
2026-07-07 21:27:00 +0700

    Add
    Menambahkan tugas

---
+++ Tugas/DivideConquerBruteForce/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace DivideConquerBruteForce
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.dgvData = new System.Windows.Forms.DataGridView();
+            this.btnBruteForce = new System.Windows.Forms.Button();
+            this.btnDnC = new System.Windows.Forms.Button();
+            this.btnNextStep = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.rtbLog = new System.Windows.Forms.RichTextBox();
+            this.btnBenchmark = new System.Windows.Forms.Button();
+            this.lblHasilBF = new System.Windows.Forms.Label();
+            this.lblHasilDC = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dgvData);
+            this.groupBox1.Location = new System.Drawing.Point(20, 20);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(750, 450);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Data & Visualisasi";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnReset);
+            this.groupBox2.Controls.Add(this.btnNextStep);
+            this.groupBox2.Controls.Add(this.btnDnC);
+            this.groupBox2.Controls.Add(this.btnBruteForce);
+            this.groupBox2.Location = new System.Drawing.Point(20, 480);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(750, 150);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Kontrol Algoritma";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.rtbLog);
+            this.groupBox3.Location = new System.Drawing.Point(790, 20);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(370, 350);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Keterangan Langkah";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.lblHasilDC);
+            this.groupBox4.Controls.Add(this.lblHasilBF);
+            this.groupBox4.Controls.Add(this.btnBenchmark);
+            this.groupBox4.Location = new System.Drawing.Point(790, 380);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(370, 250);
+            this.groupBox4.TabIndex = 3;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Uji Performa";
+            // 
+            // dgvData
+            // 
+            this.dgvData.AllowUserToAddRows = false;
+            this.dgvData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvData.Location = new System.Drawing.Point(20, 30);
+            this.dgvData.Name = "dgvData";
+            this.dgvData.ReadOnly = true;
+            this.dgvData.RowHeadersWidth = 82;
+            this.dgvData.RowTemplate.Height = 33;
+            this.dgvData.Size = new System.Drawing.Size(710, 400);
+            this.dgvData.TabIndex = 0;
+            // 
+            // btnBruteForce
+            // 
+            this.btnBruteForce.Location = new System.Drawing.Point(30, 40);
+            this.btnBruteForce.Name = "btnBruteForce";
+            this.btnBruteForce.Size = new System.Drawing.Size(200, 45);
+            this.btnBruteForce.TabIndex = 0;
+            this.btnBruteForce.Text = "Brute Force";
+            this.btnBruteForce.UseVisualStyleBackColor = true;
+            this.btnBruteForce.Click += new System.EventHandler(this.btnBruteForce_Click);
+            // 
+            // btnDnC
+            // 
+            this.btnDnC.Location = new System.Drawing.Point(260, 40);
+            this.btnDnC.Name = "btnDnC";
+            this.btnDnC.Size = new System.Drawing.Size(200, 45);
+            this.btnDnC.TabIndex = 1;
+            this.btnDnC.Text = "Divide & Conquer";
+            this.btnDnC.UseVisualStyleBackColor = true;
+            this.btnDnC.Click += new System.EventHandler(this.btnDnC_Click);
+            // 
+            // btnNextStep
+            // 
+            this.btnNextStep.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.btnNextStep.Location = new System.Drawing.Point(500, 40);
+            this.btnNextStep.Name = "btnNextStep";
+            this.btnNextStep.Size = new System.Drawing.Size(220, 45);
+            this.btnNextStep.TabIndex = 2;
+            this.btnNextStep.Text = "Next Step";
+            this.btnNextStep.UseVisualStyleBackColor = false;
+            this.btnNextStep.Click += new System.EventHandler(this.btnNextStep_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(30, 93);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(690, 45);
+            this.btnReset.TabIndex = 3;
+            this.btnReset.Text = "Reset Visualisasi";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // rtbLog
+            // 
+            this.rtbLog.Location = new System.Drawing.Point(20, 30);
+            this.rtbLog.Name = "rtbLog";
+            this.rtbLog.ReadOnly = true;
+            this.rtbLog.Size = new System.Drawing.Size(340, 400);
+            this.rtbLog.TabIndex = 0;
+            this.rtbLog.Text = "";
+            // 
+            // btnBenchmark
+            // 
+            this.btnBenchmark.Location = new System.Drawing.Point(20, 40);
+            this.btnBenchmark.Name = "btnBenchmark";
+            this.btnBenchmark.Size = new System.Drawing.Size(340, 50);
+            this.btnBenchmark.TabIndex = 0;
+            this.btnBenchmark.Text = "Benchmark";
+            this.btnBenchmark.UseVisualStyleBackColor = true;
+            this.btnBenchmark.Click += new System.EventHandler(this.btnBenchmark_Click);
+            // 
+            // lblHasilBF
+            // 
+            this.lblHasilBF.AutoSize = true;
+            this.lblHasilBF.Location = new System.Drawing.Point(25, 110);
+            this.lblHasilBF.Name = "lblHasilBF";
+            this.lblHasilBF.Size = new System.Drawing.Size(177, 25);
+            this.lblHasilBF.TabIndex = 1;
+            this.lblHasilBF.Text = "Brute Force: - ms";
+            // 
+            // lblHasilDC
+            // 
+            this.lblHasilDC.AutoSize = true;
+            this.lblHasilDC.Location = new System.Drawing.Point(25, 145);
+            this.lblHasilDC.Name = "lblHasilDC";
+            this.lblHasilDC.Size = new System.Drawing.Size(219, 25);
+            this.lblHasilDC.TabIndex = 2;
+            this.lblHasilDC.Text = "Divide & Conquer: - ms";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1280, 732);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView dgvData;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnNextStep;
+        private System.Windows.Forms.Button btnDnC;
+        private System.Windows.Forms.Button btnBruteForce;
+        private System.Windows.Forms.RichTextBox rtbLog;
+        private System.Windows.Forms.Button btnBenchmark;
+        private System.Windows.Forms.Label lblHasilDC;
+        private System.Windows.Forms.Label lblHasilBF;
+    }
+}
+
---
+++ Tugas/DivideConquerBruteForce/Form1.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace DivideConquerBruteForce
+{
+    public partial class Form1 : Form
+    {
+        // --- 1. DATA HARDCODE WAJIB ---
+        private string[] hari = { "Hari 1", "Hari 2", "Hari 3", "Hari 4", "Hari 5", "Hari 6", "Hari 7", "Hari 8", "Hari 9", "Hari 10", "Hari 11", "Hari 12", "Hari 13", "Hari 14", "Hari 15" };
+        private int[] dataSahamAwal = { 12, -15, 20, -5, 30, -10, 45, -60, 25, 10, -15, 35, -20, 15, -5 };
+
+        // --- 2. STRUKTUR DATA VISUALISASI ---
+        public class VisualStep
+        {
+            public List<int> KuningIdx { get; set; } = new List<int>();
+            public List<int> MerahIdx { get; set; } = new List<int>();
+            public List<int> HijauIdx { get; set; } = new List<int>();
+            public string Pesan { get; set; }
+        }
+
+        private List<VisualStep> listRiwayat = new List<VisualStep>();
+        private int pointerLangkah = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadDataToGrid();
+        }
+
+        private void LoadDataToGrid()
+        {
+            dgvData.Columns.Clear();
+            dgvData.Columns.Add("hari", "Hari");
+            dgvData.Columns.Add("saham", "Fluktuasi Saham");
+            for (int i = 0; i < hari.Length; i++) dgvData.Rows.Add(hari[i], dataSahamAwal[i]);
+            dgvData.ClearSelection();
+        }
+
+        // --- 3. EVENT HANDLERS (Sambungkan ini di Designer) ---
+
+        private void btnBruteForce_Click(object sender, EventArgs e)
+        {
+            ResetVisual();
+            int n = dataSahamAwal.Length;
+            int maxSoFar = int.MinValue;
+            int startMax = -1, endMax = -1;
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = i; j < n; j++)
+                {
+                    int currentSum = 0;
+                    VisualStep step = new VisualStep();
+
+                    // Hitung sum & rekam rentang kuning
+                    for (int k = i; k <= j; k++)
+                    {
+                        currentSum += dataSahamAwal[k];
+                        step.KuningIdx.Add(k);
+                    }
+
+                    if (currentSum > maxSoFar)
+                    {
+                        maxSoFar = currentSum;
+                        startMax = i; endMax = j;
+                        step.Pesan = $"[Brute Force] New Max: {maxSoFar} (Indeks {i} s/d {j})";
+                    }
+                    else step.Pesan = $"[Brute Force] Cek subarray {i}-{j}. Total: {currentSum}";
+
+                    // Rekam Hijau (Max sementara)
+                    if (startMax != -1)
+                        for (int k = startMax; k <= endMax; k++) step.HijauIdx.Add(k);
+
+                    listRiwayat.Add(step);
+                }
+            }
+            rtbLog.Text = "Brute Force recorded! Klik 'Next Step' untuk mulai.";
+        }
+
+        private void btnDnC_Click(object sender, EventArgs e)
+        {
+            ResetVisual();
+            FindMaxSubDNC(0, dataSahamAwal.Length - 1);
+            rtbLog.Text = "Divide & Conquer recorded! Klik 'Next Step' untuk mulai.";
+        }
+
+        private void btnNextStep_Click(object sender, EventArgs e)
+        {
+            if (pointerLangkah >= listRiwayat.Count)
+            {
+                MessageBox.Show("Visualisasi Selesai!");
+                return;
+            }
+
+            // Bersihkan warna lama
+            foreach (DataGridViewRow row in dgvData.Rows) row.DefaultCellStyle.BackColor = Color.White;
+
+            var step = listRiwayat[pointerLangkah];
+            rtbLog.AppendText("\n" + step.Pesan);
+            rtbLog.ScrollToCaret();
+
+            // Warnai sesuai prioritas: Kuning -> Hijau -> Merah
+            foreach (int i in step.KuningIdx) dgvData.Rows[i].DefaultCellStyle.BackColor = Color.Yellow;
+            foreach (int i in step.HijauIdx) dgvData.Rows[i].DefaultCellStyle.BackColor = Color.LightGreen;
+            foreach (int i in step.MerahIdx) dgvData.Rows[i].DefaultCellStyle.BackColor = Color.Red;
+
+            pointerLangkah++;
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            ResetVisual();
+            rtbLog.Clear();
+            rtbLog.Text = "Visualisasi di-reset.";
+        }
+
+        private void btnBenchmark_Click(object sender, EventArgs e)
+        {
+            int[] bigData = new int[10000];
+            Random r = new Random();
+            for (int i = 0; i < 10000; i++) bigData[i] = r.Next(-100, 100);
+
+            Stopwatch sw = new Stopwatch();
+
+            // BF Benchmark
+            sw.Start();
+            int bfRes = int.MinValue;
+            for (int i = 0; i < 10000; i++)
+            {
+                int sum = 0;
+                for (int j = i; j < 10000; j++)
+                {
+                    sum += bigData[j];
+                    if (sum > bfRes) bfRes = sum;
+                }
+            }
+            sw.Stop();
+            lblHasilBF.Text = $"Brute Force $O(n^2)$: {sw.ElapsedMilliseconds} ms";
+
+            // DC Benchmark
+            sw.Restart();
+            RunDCMurni(bigData, 0, 9999);
+            sw.Stop();
+            lblHasilDC.Text = $"Divide & Conquer $O(n \\log n)$: {sw.ElapsedMilliseconds} ms";
+        }
+
+        // --- 4. LOGIKA PENDUKUNG ---
+
+        private int FindMaxSubDNC(int low, int high)
+        {
+            if (low == high)
+            {
+                var step = new VisualStep { Pesan = $"Base Case: Indeks {low} ({dataSahamAwal[low]})" };
+                step.KuningIdx.Add(low);
+                listRiwayat.Add(step);
+                return dataSahamAwal[low];
+            }
+
+            int mid = (low + high) / 2;
+            var stepDiv = new VisualStep { Pesan = $"Divide: {low}-{high} -> Mid: {mid}" };
+            stepDiv.MerahIdx.Add(mid);
+            for (int i = low; i <= high; i++) stepDiv.KuningIdx.Add(i);
+            listRiwayat.Add(stepDiv);
+
+            int leftMax = FindMaxSubDNC(low, mid);
+            int rightMax = FindMaxSubDNC(mid + 1, high);
+            int crossMax = FindMaxCrossing(low, mid, high);
+
+            return Math.Max(Math.Max(leftMax, rightMax), crossMax);
+        }
+
+        private int FindMaxCrossing(int low, int mid, int high)
+        {
+            int leftSum = int.MinValue, sum = 0, maxL = mid;
+            for (int i = mid; i >= low; i--)
+            {
+                sum += dataSahamAwal[i];
+                if (sum > leftSum) { leftSum = sum; maxL = i; }
+            }
+
+            int rightSum = int.MinValue; sum = 0; int maxR = mid + 1;
+            for (int j = mid + 1; j <= high; j++)
+            {
+                sum += dataSahamAwal[j];
+                if (sum > rightSum) { rightSum = sum; maxR = j; }
+            }
+
+            var step = new VisualStep { Pesan = $"Crossing Found: {leftSum + rightSum} (Indeks {maxL}-{maxR})" };
+            step.MerahIdx.Add(mid);
+            for (int k = maxL; k <= maxR; k++) step.HijauIdx.Add(k);
+            listRiwayat.Add(step);
+
+            return leftSum + rightSum;
+        }
+
+        private int RunDCMurni(int[] arr, int low, int high)
+        {
+            if (low == high) return arr[low];
+            int mid = (low + high) / 2;
+            int l = RunDCMurni(arr, low, mid);
+            int r = RunDCMurni(arr, mid + 1, high);
+            int lS = int.MinValue, rS = int.MinValue, s = 0;
+            for (int i = mid; i >= low; i--) { s += arr[i]; if (s > lS) lS = s; }
+            s = 0;
+            for (int j = mid + 1; j <= high; j++) { s += arr[j]; if (s > rS) rS = s; }
+            return Math.Max(Math.Max(l, r), lS + rS);
+        }
+
+        private void ResetVisual()
+        {
+            pointerLangkah = 0;
+            listRiwayat.Clear();
+            foreach (DataGridViewRow row in dgvData.Rows) row.DefaultCellStyle.BackColor = Color.White;
+        }
+    }
+}
